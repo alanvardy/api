9d98e94c55101afe44b83dc29db9c45e9a792a71
Alan Vardy <alanvardy@gmail.com>
2026-07-20 16:23:17 -0700

    create files table

---
+++ migrations/20260720232439_create_files.sql
@@ -0,0 +1,12 @@
+-- Add migration script here
+CREATE TABLE IF NOT EXISTS files (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id BIGINT NOT NULL REFERENCES users(id),
+    content_type TEXT NOT NULL,
+    key TEXT NOT NULL,
+    updated_at DATETIME NOT NULL,
+    created_at DATETIME NOT NULL
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS idx_feature_flags_name ON files (key
+);
