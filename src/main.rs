efd63e87c191117cae50ece0c6e268d1f5ba639e
Alan Vardy <alanvardy@gmail.com>
2026-07-22 09:54:20 -0700

    Use templating engine

--- src/main.rs
+++ src/main.rs
@@ -30,12 +30,27 @@ async fn main() {
 }
 
 fn app(pool: sqlx::SqlitePool, env: &Env) -> Router {
+    let templates = init_templates();
     let state = AppState {
         db: pool,
         env: env.clone(),
+        templates,
     };
 
     routes::routes(env)
         .layer(log::trace_layer())
         .with_state(state)
+}
+
+fn init_templates() -> minijinja::Environment<'static> {
+    let mut templates = minijinja::Environment::new();
+    templates.set_loader(minijinja::path_loader("templates"));
+    templates.set_auto_escape_callback(|name| {
+        if name.ends_with(".html") {
+            minijinja::AutoEscape::Html
+        } else {
+            minijinja::AutoEscape::None
+        }
+    });
+    templates
 }
